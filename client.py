3f3697310c338b25a4fd1898270a57b9636e6715
Raul Wagner <raul.wagner@live.com>
2020-04-02 12:47:24 -0300

    first commit
    first commit

---
+++ client.py
@@ -0,0 +1,51 @@
+import select
+import socket
+import threading
+
+
+bufferLock = threading.Lock()
+inputBuffer = list()
+
+
+def runThread(threadName):
+    global inputBuffer
+
+    while True:
+        try:
+            inputMsg = input()
+            bufferLock.acquire()
+            inputBuffer.insert(0, inputMsg)
+            bufferLock.release()
+        except EOFError:
+            print(EOFError)
+            break
+
+
+HOST = '192.168.0.12'  # The remote host
+PORT = 5555  # The same port as used by the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+client.setblocking(False)
+
+lgn = input("What is your name? ")
+
+client.send(lgn.encode("utf-8"))
+
+t1 = threading.Thread(target = runThread, args=("nomeThread", ))
+t1.start()
+
+print("say hi! :D")
+
+while True:
+    bufferLock.acquire()
+    if len(inputBuffer) > 0:
+        inputMsg = inputBuffer.pop()
+        inputMsg = lgn + ": " + inputMsg
+        client.send(inputMsg.encode("utf-8"))
+    bufferLock.release()
+
+    ready = select.select([client], [], [], 0.3)
+    if ready[0]:
+        data = client.recv(1024 * 16)
+        print(data.decode("utf-8"))
+
---
+++ server.py
@@ -0,0 +1,67 @@
+import socket
+from _thread import *
+
+
+def threaded_client(conn, id):
+    global availableClients
+    global numClients
+    while True:
+        try:
+            newData = conn.recv(1024 * 16)
+            if not newData:
+                break
+            if startedConversation and newData:
+                msg = newData.decode("utf-8")
+                print("received msg: " + msg)
+                availableClients[(id + 1) % 2].send(msg.encode("utf-8"))
+        except:
+            numClients -= 1
+            availableClients[id] = None
+            break
+    conn.close()
+
+
+server = "192.168.0.12"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+availableClients = list()
+availableClients.append(None)
+availableClients.append(None)
+
+numClients = 0
+startedConversation = False
+
+while True:
+    try:
+        conn, addr = s.accept()
+        print("Connected to:", addr)
+        data = conn.recv(1024)
+        if not data:
+            break
+        login = data.decode("utf-8")
+        print("received login: " + login + "\n")
+
+        availableClients[numClients] = conn
+
+        print("started a new thread...\n")
+        start_new_thread(threaded_client, (conn, numClients))
+        numClients += 1
+        if numClients == 2:
+            print("conversation started...")
+            startedConversation = True
+
+        else:
+            print("Waiting other client...")
+    except socket.error as err:
+        print(err)
+        break
